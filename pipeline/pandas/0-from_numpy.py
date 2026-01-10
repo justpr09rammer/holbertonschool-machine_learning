57aa745dee6e89113477722a0e760b1284245cc3
justpr09rammer <nihadqocayev152@gmail.com>
2026-01-10 22:27:08 +0000

    updated task 0

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""
+Defines function that creates a Pandas DataFrame from a Numpy ndarray
+"""
 import pandas as pd
-
 def from_numpy(array):
      """
     function to create a dataframe
@@ -8,7 +10,6 @@ def from_numpy(array):
         array: np.ndarray from which you should create the pd.DataFrame
     Returns: newly created pd.DataFrame
     """
-
     m = array.shapre[1]
     cols = list(string.ascii_uppercase[:m])
     return pd.DataFrame(array, columns=cols)
