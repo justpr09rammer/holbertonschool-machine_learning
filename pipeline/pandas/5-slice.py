e8850c6e491781d57bb728f41b17ae42fe5ad3dd
justpr09rammer <nihadqocayev152@gmail.com>
2026-01-10 22:59:34 +0000

    task 5

---
+++ pipeline/pandas/5-slice.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Function that slices a DataFrame:
+- keeps only High, Low, Close, Volume_BTC
+- selects every 60th row
+"""
+
+
+def slice(df):
+    """
+    Args:
+        df (pd.DataFrame): input dataframe
+    Returns:
+        pd.DataFrame: sliced dataframe
+    """
+    # 1. Select required columns
+    df_selected = df[['High', 'Low', 'Close', 'Volume_BTC']]
+    # 2. Select every 60th row
+    df_sliced = df_selected.iloc[::60]
+    return df_sliced
