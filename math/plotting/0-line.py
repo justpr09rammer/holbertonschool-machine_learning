c2fcc2c5b116c640e93f1061f016395451600bd4
justpr09rammer <nihadqocayev152@example.com>
2026-01-11 11:43:00 +0400

    corrected task 0

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -16,12 +16,9 @@ def line():
     """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-
     # Plot y with implicit x (0 to 10)
     plt.plot(y, 'r-')
-
     # Set x-axis range exactly from 0 to 10
     plt.xlim(0, 10)
-
     plt.show()
 
