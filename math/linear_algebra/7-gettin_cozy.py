6cab5d92e2dd423ae17df3b62117cb9262089ddb
justpr09rammer <nihadqocayev152@gmail.com>
2026-02-01 10:06:32 +0000

    reamining tasks completed

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Function that concatenates two matrices along a specific axis.
+"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """
+    Concatenates two 2D matrices along a specific axis.
+
+    Parameters:
+    mat1 (list of list of ints/floats): First matrix.
+    mat2 (list of list of ints/floats): Second matrix.
+    axis (int): Axis along which to concatenate the matrices (0 or 1).
+
+    Returns:
+    list of list of ints/floats: New matrix resulting from concatenatenation.
+    None: If the concatenation is not posible due to dimension mismatch.
+    """
+    if not mat1 and not mat2:
+        return []
+    if not mat1:
+        return mat2[:] if axis == 0 else None
+    if not mat2:
+        return mat1[:] if axis == 0 else None
+
+    if axis == 0:
+        # Check if both matrices have the same number of columns
+        if len(mat1[0]) == len(mat2[0]):
+            return mat1 + mat2
+        else:
+            return None
+    elif axis == 1:
+        # Check if both matrices have the same number of rows
+        if len(mat1) == len(mat2):
+            return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+        else:
+            return None
+
+    return None
