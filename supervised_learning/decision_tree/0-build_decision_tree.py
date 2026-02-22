401b9e7ee9cc561eba5c396a2de79cf7df00f5c1
justpr09rammer <nihadqocayev152gmail.com>
2026-02-22 11:04:27 +0000

    added documentation

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 
+
 class Node:
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
@@ -15,7 +16,7 @@ class Node:
         self.depth = depth
 
     def max_depth_below(self):
-        # If node has no children, return its depth
+        """Return the maximum depth below this node."""
         if self.left_child is None and self.right_child is None:
             return self.depth
 
@@ -39,10 +40,11 @@ class Leaf(Node):
         self.depth = depth
 
     def max_depth_below(self):
+        """Return the depth of the leaf."""
         return self.depth
 
 
-class Decision_Tree():
+class Decision_Tree:
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
         self.rng = np.random.default_rng(seed)
@@ -58,4 +60,5 @@ class Decision_Tree():
         self.predict = None
 
     def depth(self):
+        """Return the maximum depth of the decision tree."""
         return self.root.max_depth_below()
