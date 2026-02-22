98d139f4d1f97436eef074d755d676e5c4e0e333
justpr09rammer <nihadqocayev152gmail.com>
2026-02-22 11:05:30 +0000

    task 9 corrected

--- supervised_learning/decision_tree/0-build_decision_tree.py
+++ supervised_learning/decision_tree/0-build_decision_tree.py
@@ -1,18 +1,3 @@
-#!/usr/bin/env python3
-
-import numpy as np
-
-
-class Node:
-    def __init__(self, feature=None, threshold=None, left_child=None,
-                 right_child=None, is_root=False, depth=0):
-        self.feature = feature
-        self.threshold = threshold
-        self.left_child = left_child
-        self.right_child = right_child
-        self.is_leaf = False
-        self.is_root = is_root
-        self.sub_population = None
         self.depth = depth
 
     def max_depth_below(self):
@@ -21,7 +6,7 @@ class Node:
             return self.depth
 
         left_depth = self.depth
-        right_depth = self.depth
+        right_depth = self.depth     
 
         if self.left_child is not None:
             left_depth = self.left_child.max_depth_below()
@@ -33,20 +18,26 @@ class Node:
 
 
 class Leaf(Node):
+    """Represents a leaf node of a decision tree."""
+
     def __init__(self, value, depth=None):
+        """Initialize a leaf node."""
         super().__init__()
         self.value = value
-        self.is_leaf = True
+        self.is_leaf = True  
         self.depth = depth
 
     def max_depth_below(self):
         """Return the depth of the leaf."""
-        return self.depth
+        return self.depth      
 
 
 class Decision_Tree:
+    """Represents a decision tree."""
+
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
+        """Initialize the decision tree."""
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -57,8 +48,9 @@ class Decision_Tree:
         self.max_depth = max_depth
         self.min_pop = min_pop
         self.split_criterion = split_criterion
-        self.predict = None
+        self.predict = None     
 
     def depth(self):
         """Return the maximum depth of the decision tree."""
         return self.root.max_depth_below()
+                                                                                                                                                                                    137,42        Bot
