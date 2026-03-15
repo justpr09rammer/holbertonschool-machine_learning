51aad4420c38312971364dc77746671deaf8711e
justpr09rammer <nihadqocayev152@gmail.com>
2026-03-15 15:46:08 +0000

    task 2 and task 4

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -1,19 +1,30 @@
 #!/usr/bin/env python3
 """
-    Keras project (finally)
+Defines a function that sets up Adam optimizer for Keras model
+with categorical crossentropy loss and accuracy metrics
 """
+
+
 import tensorflow.keras as K
 
 
 def optimize_model(network, alpha, beta1, beta2):
-    """ Sets up Adam optimization for keras model with categorical
-        crossentropy loss and accuracy metrics
-        network is model to optimize
-        alpha is learning rate
-        beta1 is first Adam optimization parameter
-        beta2 is second adam optmization parameter
-        Returns: None
     """
-    A = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
-    network.compile(loss='categorical_crossentropy', optimizer=A,
+    Sets up Adam optimizer for Keras model with
+        categorical crossentropy loss and accuracy metrics
+
+    parameters:
+        network [keras model]: model to optimize
+        alpha [float]: learning rate
+        beta1: first Adam optimization parameter
+        beta2: second Adam optimization parameter
+
+    returns:
+        None
+    """
+    network.compile(optimizer=K.optimizers.Adam(lr=alpha,
+                                                beta_1=beta1,
+                                                beta_2=beta2),
+                    loss='categorical_crossentropy',
                     metrics=['accuracy'])
+    return None
--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python3
 """
-    Keras project (finally)
+Defines a function that trains a model using mini-batch gradient descent
+using Keras library
 """
+
+
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size,
-                epochs, verbose=True, shuffle=False):
-    """ Trains a model using mini-bath gradient descent
-        now using keras api in tensorflow
-        network is model to train
-        data is ndarray (m, nx)
-        labels is one-hot ndarray (m, classes), labels of data
-        batch_size is size of batches
-        epochs is numbber of passes of gradient descent on every example
-        verbose is bool for printing during training
-        shuffle is bool whether to shuffle at each epoch
-        Returns: History object generated after training
+def train_model(network, data, labels, batch_size, epochs,
+                verbose=True, shuffle=False):
     """
-    history = network.fit(data, labels, epochs=epochs,
+    Trains a model using mini-batch gradient descent with Keras
+
+    parameters:
+        network [keras model]: model to train
+        data [numpy.ndarray of shape (m, nx)]:
+            contains the input data
+        labels [one-hot numpy.ndarray of shape (m, classes)]:
+            contains labels of data
+        batch_size [int]:
+            size of batch used for mini-batch gradient descent
+        epochs [int]:
+            number of passes through data for mini-batch gradient descent
+        verbose [boolean]:
+            determines if output should be printed during training
+        shuffle [boolean]:
+            determines whether to shuffle the batches every epoch
+
+    returns:
+        the History object generated after training the model
+    """
+    history = network.fit(x=data, y=labels,
                           batch_size=batch_size,
-                          verbose=verbose, shuffle=shuffle)
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle)
     return history
